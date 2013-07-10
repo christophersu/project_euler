7e1240d18605498570ea8b7a781b2325b9a499c4
Christopher Su <christophersu9@gmail.com>
2013-07-10 17:19:28 +0800

    fixed spacing

--- Problem 3/Main.java
+++ Problem 3/Main.java
@@ -18,13 +18,13 @@ public class Main {
 	}
 	
 	public static boolean isPrime(int n) {
-        if ((n <= 1) || (n % 2 == 0)) return false;
-        if (n == 2)return true;
-        for (int i = 3; i <= Math.sqrt(n) + 1; i = i + 2) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+		if ((n <= 1) || (n % 2 == 0)) return false;
+		if (n == 2)return true;
+		for (int i = 3; i <= Math.sqrt(n) + 1; i = i + 2) {
+		    if (n % i == 0) {
+		        return false;
+		    }
+		}
+		return true;
+	}
 }
