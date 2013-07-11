1a5e21c99df8865c7742cd19dac7cfdbb9e7d218
Christopher Su <christophersu9@gmail.com>
2013-07-11 15:19:16 +0800

    problem 12 UNSOLVED

---
+++ Problem 12/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		int triangleCounter = 7;
+		int triangleNumber = 28;
+		while (numberOfDivisors(triangleNumber) < 501) {
+			triangleCounter++;
+			triangleNumber = triangleNumber + triangleCounter;
+		}
+	}
+	
+	public static int numberOfDivisors(int x) {
+		int divisors = 0;
+		int inc = 1;
+		if (x % 2 != 0) inc = 2;
+		for (int c = 1; c <= x / 2; c = c+inc) {
+			if (x % c == 0) divisors++;
+		}
+		return divisors;
+	}
+}
