6372fb704c49f039db4bbcbc48dba0830496b199
Christopher Su <christophersu9@gmail.com>
2013-07-13 16:14:20 +0800

    problem 28 solved. I liked this one :)

---
+++ Problem 028/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		int current = 1;
+		int increment = 2;
+		int sum = 1;
+		for (int x = 0; x < 500; x++) {
+			for (int y = 0; y < 4; y++) {
+				current += increment;
+				sum += current;
+			}
+			increment += 2;
+		}
+		System.out.println(sum);
+	}
+}
