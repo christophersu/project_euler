eb159281f05d578d6d242fe9953744f598369a92
Christopher Su <christophersu9@gmail.com>
2013-07-13 17:24:31 +0800

    problem 24 started

---
+++ Problem 024/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+// Lexicographic permutations
+// Problem 24
+
+public class Main {
+	public static void main(String[] args) {
+		ArrayList<String> permutations = new ArrayList<String>();
+		String temp = "";
+		for (int a = 0; a < 10; a++) {
+			for (int b = 0; b < 10; b++) {
+				if (a == b) {}
+				else {
+					for (int c = 0; c < 10; c++) {
+						if (a == b) {}
+						else {
+							
+						}
+				}
+			}
+		}
+		for (String x : permutations) System.out.println(x);
+	}
+}
